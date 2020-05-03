fda77272bf5c1eee4f89c38a2af453757065a8d3
EyalGreen <grineyal@gmail.com>
2020-05-03 09:21:52 +0300

    Finished up XML event filter to use constants and work with inner elements.

--- src/main/java/filters/XMLElementFilter.java
+++ src/main/java/filters/XMLElementFilter.java
@@ -3,34 +3,54 @@ package filters;
 import java.io.IOException;
 
 public class XMLElementFilter extends FilterBooleanLogic {
+    private static final char LESS_THAN_CHARACTER = '<';
+    private static final char GREATER_THAN_CHARACTER = '>';
+    private static final char SLASH_CHARACTER = '/';
     private String acceptedElement;
     private StringBuilder currentElement;
     private boolean inElementTag;
-    private boolean inStartElement;
+    private boolean inAcceptedElement;
+
 
     public XMLElementFilter(StatefulFilterInputStream filterInputStream, String acceptedElement) {
         super(filterInputStream);
         this.acceptedElement = acceptedElement;
         currentElement = new StringBuilder();
         inElementTag = false;
+        inAcceptedElement = false;
     }
-    // Does not handle inner elements yet
+
+    /**
+     * The XML event filter read method detects whether the current characters being read are
+     * inside of an element tag or not, whilst keeping the state of whether or not the characters
+     * are inside of the accepted element given to the filter. Only when the state corresponds
+     * to being in the boundries of the accepted tag, and not inside any inner tag, will the filter change
+     * its state to accepting.
+     */
     public int read() throws IOException {
-        int charvalue = filterInputStream.read();
-        char ch = (char) charvalue;
-        if (ch == '<') {
-            inElementTag = true;
-            currentElement.setLength(0);
-        } else if (ch == '>') {
-            inElementTag = false;
-        } else if (inElementTag && ch != '/') {
-            currentElement.append(ch);
-        } else if (inElementTag) { // Enters when in element tag and character is slash
-            currentElement.setLength(0);
+        int charValue = filterInputStream.read();
+        char ch = (char) charValue;
+        isAccepting = inAcceptedElement && !inElementTag;
+        if ((inElementTag && ch == GREATER_THAN_CHARACTER) || (!inElementTag && ch == LESS_THAN_CHARACTER)) {
+            isAccepting = false;
         }
-        isAccepting = (currentElement.toString().equals(acceptedElement));
-        if (!inElementTag && ch != '>')
-            isAccepting = false; // take care of this
-        return charvalue;
+        if (ch == LESS_THAN_CHARACTER) {
+            inElementTag = true;
+            resetCurrentElement();
+        } else if (ch == GREATER_THAN_CHARACTER) {
+            inElementTag = false;
+            if (currentElement.toString().equals(acceptedElement)) {
+                inAcceptedElement = !inAcceptedElement;
+            }
+        } else if (inElementTag && ch == SLASH_CHARACTER) {
+            resetCurrentElement();
+        } else if (inElementTag) {
+            currentElement.append(ch);
+        }
+        return charValue;
+    }
+
+    private void resetCurrentElement() {
+        this.currentElement.setLength(0);
     }
 }
